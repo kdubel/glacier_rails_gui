e6590886113dfd7004ad5c385b2b808bca218638
Kamil Dubel <kamil@themasters.io>
2019-03-21 16:04:55 +0100

    changed unicorn config (listen on port)

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -4,8 +4,8 @@ working_directory "#{app_path}/current"
 pid               "#{app_path}/current/tmp/pids/unicorn.pid"
 
 # listen on port
-#listen 3000, backlog: 64
-listen '/srv/http/glacier_gui/shared/tmp/unicorn.sock', backlog: 64
+listen 3000, backlog: 64
+#listen '/srv/http/glacier_gui/shared/tmp/unicorn.sock', backlog: 64
 
 # logging
 stderr_path 'log/unicorn.stderr.log'
